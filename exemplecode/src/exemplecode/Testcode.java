664c005a98456c06680068088e07046689aff75c
Camilo <Camilo@Lenovo-PC>
2020-05-29 19:42:22 -0400

    je ecrit une deuxieme message

---
+++ exemplecode/src/exemplecode/Testcode.java
@@ -0,0 +1,10 @@
+package exemplecode;
+
+public class Testcode {
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde");
+		System.out.println("Hola a todos");
+	}
+
+}
